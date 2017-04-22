21c838f10ce7580b6fd90c169ea159ba6853cf83
Yuujin <zen@melnik.lt>
2017-04-22 19:38:48 +0100

    World movements

---
+++ Assets/WorldNavigation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class WorldNavigation : MonoBehaviour {
+
+    public float speed;
+
+    void Start () {
+    }
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            RotateXBy(-speed);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            RotateXBy(speed);
+        }
+    }
+
+    void RotateXBy(float x) {
+        transform.Rotate(new Vector3(x, 0, 0) * Time.deltaTime);
+    }
+}
