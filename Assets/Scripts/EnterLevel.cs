51cae53e5c399fc2fb9148bf44032fe1400d3604
Yuujin <zen@melnik.lt>
2017-04-23 04:22:57 +0100

    Clickable level-gate objects with popup animation. Renamed layers

---
+++ Assets/Scripts/EnterLevel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterLevel : MonoBehaviour {
+
+    public float speed;
+    public float stopZPosition;
+
+    void Start() {
+    }
+
+    void Update() {
+    }
+
+    void OnMouseDown() {
+        StartCoroutine("PopUp");
+    }
+
+    IEnumerator PopUp() {
+        while (transform.localPosition.z < stopZPosition - 0.001f)
+        {
+            transform.localPosition = Vector3.Lerp(
+                transform.localPosition, 
+                new Vector3(transform.localPosition.x, transform.localPosition.y, stopZPosition),
+                speed * 0.01f
+            );
+
+            yield return null;
+        }
+    }
+
+}
