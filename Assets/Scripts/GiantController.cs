15d08fe14220789bd6dcddd7db00dc0da294e467
Yuujin <zen@melnik.lt>
2017-04-23 13:54:34 +0100

    Added animation with new giant sprite. Put quick audio on the background.

---
+++ Assets/Scripts/GiantController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiantController : MonoBehaviour {
+
+    private Animator animator;
+
+    void Start() {
+        animator = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            animator.SetInteger("Direction", 1);
+            animator.SetBool("Moving", true);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            animator.SetInteger("Direction", 2);
+            animator.SetBool("Moving", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            animator.SetInteger("Direction", 0);
+            animator.SetBool("Moving", false);
+        }
+    }
+}
