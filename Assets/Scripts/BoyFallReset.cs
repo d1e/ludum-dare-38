084de658fbdee432beb8afebe66aa6c208856110
Yuujin <zen@melnik.lt>
2017-04-25 00:12:48 +0100

    Added giant head sprite. Boy fall and return

---
+++ Assets/Scripts/BoyFallReset.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoyFallReset : MonoBehaviour
+{
+
+    public string platformName;
+
+    void Start()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D boy)
+    {
+        Debug.Log("fell");
+        if (boy.tag == "boy")
+        {
+            GameObject resetTo = GameObject.Find(platformName);
+            Debug.Log(resetTo);
+            boy.transform.position = new Vector3(
+                resetTo.transform.position.x - 5, 
+                resetTo.transform.position.y + 5,
+                boy.transform.position.z
+            );
+        }
+    }
+}
