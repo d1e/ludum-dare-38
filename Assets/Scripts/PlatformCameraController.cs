bc0893c5479960872c1e4de80a2713b9a5c4f7c8
Yuujin <zen@melnik.lt>
2017-04-24 22:50:56 +0100

    Camera with X bounds for platform; hardcoded min/max X pos config

---
+++ Assets/Scripts/PlatformCameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformCameraController : MonoBehaviour
+{
+
+    public GameObject boy;
+    public float minPosX;
+    public float maxPosX;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(
+            getXPositionInLevelBounds(),
+            transform.position.y,
+            transform.position.z
+        );
+    }
+
+    private float getXPositionInLevelBounds()
+    {
+        if (boy.transform.position.x <= minPosX)
+        {
+            return minPosX;
+        }
+        else if (boy.transform.position.x >= maxPosX)
+        {
+            return maxPosX;
+        }
+        else
+        {
+            return boy.transform.position.x;
+        }
+    }
+}
