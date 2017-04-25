03236e6de88a031fbecd2868a9b4a6959fb86b07
Yuujin <zen@melnik.lt>
2017-04-25 04:07:41 +0100

    added moon to exit the level

---
+++ Assets/PickShards.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickShards : MonoBehaviour
+{
+   
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "boy")
+        {
+            this.enabled = false;
+            GameManagement gm = GameObject.Find("game manager").GetComponent<GameManagement>();
+            StartCoroutine(gm.iSyncLevelOut("boylevelone"));
+        }
+    }
+}
